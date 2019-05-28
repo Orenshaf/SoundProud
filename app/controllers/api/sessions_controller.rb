eca16ccb394a5c5e7f873a767c7b9d9b0c772e72
orenshaf <orenshaf@gmail.com>
2019-05-28 18:11:01 -0400

    add custom route to check email

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,15 @@
 class Api::SessionsController < ApplicationController
+
+    def check_email
+        @user = User.find_by(email: params[:user][:email])
+
+        if @user
+            render "api/session/show"
+        else
+            render json: ['That profile url does not exist'], status: 422
+        end
+    end
+
     def create
         @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
 
