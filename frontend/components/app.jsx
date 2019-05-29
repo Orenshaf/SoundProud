a25b9d8c17915134c28478d5772ddea19a79f01f
orenshaf <orenshaf@gmail.com>
2019-05-29 18:17:22 -0400

    comment out modal to push to heroku

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Splash from './splash';
-import Modal from './modal';
+// import Modal from './modal';
 
 const App = () => (
     <div className="app">
-        <Modal />
+        {/* <Modal /> */}
         <Splash />
     </div>
 );
