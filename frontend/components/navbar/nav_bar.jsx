608ca1dd012b087cf79b13dbae3ae9abca02bc33
orenshaf <orenshaf@gmail.com>
2019-06-01 18:50:31 -0400

    fix dots

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -34,7 +34,7 @@ class NavBar extends React.Component {
                         <div className="dropdown">
                             <button id="dropDownButton" className="nav-bar-button notification-button" onClick={this.openDropDown}><img className="notification-img" src={window.dotsIcon} /></button>
                             <div id="myDropdown" className="dropdown-content">
-                                <button className="nav-bar-button logout nav-bar-logout" onClick={this.handleLogout}>Log Out</button>
+                                <button className="nav-bar-button logout" id="nav-bar-logout" onClick={this.handleLogout}>Log Out</button>
                             </div>
                         </div>
                     </div>
