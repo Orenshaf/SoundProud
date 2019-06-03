0fd0300a04b01e1d8bf5a852a0dd0913a25be05e
orenshaf <orenshaf@gmail.com>
2019-06-03 16:50:28 -0400

    remove users show page for now

--- app/views/api/users/_user.json.jbuilder
+++ app/views/api/users/_user.json.jbuilder
@@ -1,4 +1,4 @@
 json.extract! user, :id, :email, :username
-json.tracks do
-    json.array! user.tracks, :id
-end
+# json.tracks do
+#     json.array! user.tracks, :id
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create, :show, :update]
+    resources :users, only: [:create, :update]
     resources :tracks, only: [:index, :create, :show, :destroy, :update]
     resource :session, only: [:create, :destroy] 
     get 'users/check_email', to: 'users#check_email'
