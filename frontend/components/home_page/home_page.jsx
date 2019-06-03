e6eb11dc6171dc854b4138c758e890bc2bee750b
orenshaf <orenshaf@gmail.com>
2019-06-03 11:57:03 -0400

    fix navbar using navlinks

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class HomePage extends React.Component {
     constructor(props) {
         super(props);
-        debugger
         this.handleLogout = this.handleLogout.bind(this);
     }
     
@@ -20,7 +19,6 @@ class HomePage extends React.Component {
         const tracks = this.props.tracks ? this.props.tracks.map( track => {
             return <li key={track.id}>{track.trackUrl}</li>
         }) : null ;
-        debugger;
         return (
             <div className="track-index">
                 <ul>
--- frontend/components/tracks/track_form.jsx
+++ frontend/components/tracks/track_form.jsx
@@ -14,7 +14,11 @@ class TrackForm extends React.Component {
         this.handleFile = this.handleFile.bind(this);
     }
 
-    handleFormRender(){
+    handleFile(e) {
+        this.setState({photoFile: e.currentTarget.files[0]})
+    }
+    
+    handleFormRender() {
         if (this.state.trackFile) {
             return (
                 <div></div>
@@ -30,14 +34,9 @@ class TrackForm extends React.Component {
                         Provide FLAC, WAV, ALAC or AIFF for best audio quality. Learn more about high quality audio (HQ).
                     </div>
                 </div>
-            ) 
+            )
         }
     }
-
-    handleFile(e) {
-        this.setState({photoFile: e.currentTarget.files[0]})
-    }
-    
 
     render() {
         return this.handleFormRender();
