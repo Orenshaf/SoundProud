a528dd53fd5d8849641dab95559bd535efb8b368
orenshaf <orenshaf@gmail.com>
2019-06-07 00:47:34 -0400

    remove all debuggers

--- frontend/components/logout_page/logout_page.jsx
+++ frontend/components/logout_page/logout_page.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class LogoutPage extends React.Component {
     
     componentDidMount() {
-        debugger
         if (this.props.currentUser) {
             this.props.logout()
         }
