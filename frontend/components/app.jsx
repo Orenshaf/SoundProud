20960380d31e5218a9e0352398423c7b48e6fc27
orenshaf <orenshaf@gmail.com>
2019-06-07 13:47:49 -0400

    fix logout

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -19,7 +19,7 @@ const App = () => (
         <div className="app">
             <Modal />
             <Switch>
-                <Route path="/logout" component={LogoutPageContainer} />
+                <Route exact path="/logout" component={LogoutPageContainer} />
                 <Route path="/upload" component={UploadPageContainer} />
                 <Route path="/discover" component={HomePageContainer} />
                 <Route path="/:trackId" component={TrackShowPageContainer} />
