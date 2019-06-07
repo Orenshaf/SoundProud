42d4783d5703fcd887f3038f5a658ca200dbf872
orenshaf <orenshaf@gmail.com>
2019-06-07 00:58:09 -0400

    add logout to logout page container

--- frontend/components/logout_page/logout_page_container.jsx
+++ frontend/components/logout_page/logout_page_container.jsx
@@ -1,5 +1,6 @@
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { logout } from '../../actions/session_actions';
 import LogoutPage from './logout_page';
 
 const msp = (state, ownProps) => ({
