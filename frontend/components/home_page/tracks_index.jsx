aa7bff475b2646c1677f1c4d288aad80287c56e1
orenshaf <orenshaf@gmail.com>
2019-06-26 20:57:42 -0400

    reset waveform

--- frontend/components/home_page/tracks_index.jsx
+++ frontend/components/home_page/tracks_index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import TrackItem from './track_item';
 
-const TracksIndex = ({tracks, fetchCurrentTrack, history}) => {
-    const tracksIndex = tracks.map(track => {
+const TracksIndex = ({ tracks, fetchCurrentTrack, history, limit }) => {
+    const tracksIndex = tracks.slice(0, limit).map(track => {
         return (
-            <TrackItem 
-                key={track.id} 
-                trackId={track.id} 
-                photo={track.photoUrl} 
-                title={track.title} 
-                fetchCurrentTrack={fetchCurrentTrack} 
-                history={history} 
+            <TrackItem
+                key={track.id}
+                trackId={track.id}
+                photo={track.photoUrl}
+                title={track.title}
+                fetchCurrentTrack={fetchCurrentTrack}
+                history={history}
             />
         )
     })
