39fb8e79935dfa7a70bc993f906c76cd0bc522f0
orenshaf <orenshaf@gmail.com>
2019-06-26 23:03:55 -0400

    splash page has tracks and footer;

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import TracksIndex from '../home_page/tracks_index';
+
 
 class Splash extends React.Component {
     constructor(props){
@@ -12,6 +14,7 @@ class Splash extends React.Component {
 
     componentDidMount () {
         this.intervalID = setInterval(() => this.setState({header: !this.state.header}), 5000)
+        this.props.fetchTracks();
     }
 
     componentWillUnmount(){
@@ -19,33 +22,48 @@ class Splash extends React.Component {
     }
 
     render () {
+        const fetchCurrentTrack = this.props.fetchCurrentTrack;
+        const tracks = this.props.tracks.length > 0 ? <TracksIndex tracks={this.props.tracks} history={this.props.history} fetchCurrentTrack={fetchCurrentTrack} limit={6}/> : null;
         return (
             <>
-                <div className="header-top">
+                <div className="splash-page">
+                    <div className="header">
+                        <div className="header-top">
 
 
-                    <span className="header-name"><img src={window.soundCloudIcon} /><span id="soundproud-splash">SOUNDPROUD</span></span>
+                            <span className="header-name"><img src={window.soundCloudIcon} /><span id="soundproud-splash">SOUNDPROUD</span></span>
 
-                    <div className="header-nav">
-                        <button onClick={() => this.props.openModal('loginInfo')} className="splash-button sign-in">Sign in</button>
-                        <button onClick={() => this.props.openModal('loginInfo')} className="splash-button create-account">Create account</button>
-                        {/* <button className="splash-button welcome">For Creators</button> */}
-                    </div>
-                </div>
-                <div className={`${this.state.header ? "splash-header-one" : "splash-header-two"}`}>
+                            <div className="header-nav">
+                                <button onClick={() => this.props.openModal('loginInfo')} className="splash-button sign-in">Sign in</button>
+                                <button onClick={() => this.props.openModal('loginInfo')} className="splash-button create-account">Create account</button>
+                                {/* <button className="splash-button welcome">For Creators</button> */}
+                            </div>
+                        </div>
+                        <div className={`${this.state.header ? "splash-header-one" : "splash-header-two"}`}>
 
-                    <div className="header-mid">
+                            <div className="header-mid">
 
-                        <div className="header-content">
+                                <div className="header-content">
 
-                            <h1>Discover more music on SoundProud</h1>
-                            <p>SoundProud lets you listen to music wherever you are, with over 150 million tracks — and growing.</p>
-                            <button className="splash-button demo-login" onClick={() => this.props.openModal('loginInfo')}>Start Demo Login</button>
+                                    <h1>Discover more music on SoundProud</h1>
+                                    <p>SoundProud lets you listen to music wherever you are, with over 150 million tracks — and growing.</p>
+                                    <button className="splash-button demo-login" onClick={() => this.props.openModal('loginInfo')}>Start Demo Login</button>
+
+                                </div>
+
+                            </div>
 
                         </div>
-
                     </div>
-
+                    <h1 className="splash-title">Hear what’s trending in the SoundProud community</h1>
+                    <div className="splash-track-index">
+                        {tracks}
+                    </div>
+                    <div className="footer">
+                        <a className="github" href="https://github.com/Orenshaf"><i className="devicon-github-plain"></i></a>
+                        <a className="linked-in" href="https://www.linkedin.com/in/orenshafir/"><i className="fab fa-linkedin-in"></i></a>
+                        <div className="copyright">Copyright &copy; 2019 - Oren Shafir</div>
+                    </div>
                 </div>
             </>
         )
--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
-import { openModal } from '../../actions/modal_actions'
+import { openModal } from '../../actions/modal_actions';
+import { fetchTracks, fetchCurrentTrack } from '../../actions/track_actions';
 import Splash from './splash';
 
-const msp = ({ session }) => ({
-    currentUser: session.currentUser
+const msp = ({ session, entities }) => ({
+    currentUser: session.currentUser,
+    tracks: Object.values(entities.tracks),
+
 });
 
 const mdp = dispatch => ({
     openModal: modal => dispatch(openModal(modal)),
+    fetchTracks: () => dispatch(fetchTracks()),
 });
 
 export default connect(msp, mdp)(Splash);
