12e4d506dfa462da564559ce517a30f28c1d9f02
orenshaf <orenshaf@gmail.com>
2019-07-03 12:27:09 -0400

    comments sort of work

--- app/controllers/api/tracks_controller.rb
+++ app/controllers/api/tracks_controller.rb
@@ -1,7 +1,7 @@
 class Api::TracksController < ApplicationController
 
     def index
-        @tracks = Track.all.with_attached_photo.with_attached_track_file.includes(:user).includes(:comments)
+        @tracks = Track.all.with_attached_photo.with_attached_track_file.includes(:user)
         render :index
     end
     
@@ -15,7 +15,7 @@ class Api::TracksController < ApplicationController
     end
 
     def show
-        @track = Track.find_by(id: params[:id])
+        @track = Track.includes(:comments).find_by(id: params[:id])
 
         if @track 
             render "api/tracks/show"
