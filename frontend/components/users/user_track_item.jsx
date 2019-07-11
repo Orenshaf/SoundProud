abe06e9604576401d3826e60819c1a1ec29bc426
orenshaf <orenshaf@gmail.com>
2019-07-11 14:39:16 -0400

    everything but waveform and comments on user show page

--- frontend/components/users/user_track_item.jsx
+++ frontend/components/users/user_track_item.jsx
@@ -3,6 +3,12 @@ import TrackPlayButton from '../../components/tracks/track_play_button';
 
 const UserTrackItem = ({track}) =>  {
     const photo = <img className="user-showpage-track-item-photo" src={track.photoUrl} />
+    
+    let createdAt;
+    const timeNow = Date.now();
+    const createdTime = new Date(track.created_at);
+    createdAt = Math.floor((timeNow - createdTime) / (1000 * 60 * 60 * 24));
+    createdAt = createdAt < 1 ? <h1 className="showpage-track-time-stamp">less than one day ago</h1> : <h1 className="showpage-track-time-stamp">{`${createdAt} days ago`}</h1>;
     return (
         <div className="user-showpage-track-item">
             {photo}
@@ -11,15 +17,15 @@ const UserTrackItem = ({track}) =>  {
                     <div className="user-showpage-track-item-left">
                         <TrackPlayButton trackId={track.id} playButton={true} class={"small"}/>
                        <div className="user-showpage-track-item-trackinfo">
-                           <p>
+                            <p className="showpage-username">
                                 {track.username}
-                           </p>
-                           <p>
+                            </p>
+                            <p className="showpage-title">
                                 {track.title}
-                           </p>
+                            </p>
                        </div>
                     </div>
-
+                    {createdAt}
                 </div>
             </div>
         </div>
