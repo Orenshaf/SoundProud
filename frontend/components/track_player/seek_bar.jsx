73a8e13a2d767d89476807c8a1a4ca1a011e7d6a
orenshaf <orenshaf@gmail.com>
2019-07-13 15:45:44 -0400

    waveform works on user-pages

--- frontend/components/track_player/seek_bar.jsx
+++ frontend/components/track_player/seek_bar.jsx
@@ -24,11 +24,14 @@ class SeekBar extends React.Component {
     handlePercentage(e) {
         let newPercentage
         const windowSize = ((window.innerWidth - 1280) / 2);
-        if (this.props.seekBarStyle) {
-            newPercentage = Math.floor((((e.clientX - windowSize) - (e.currentTarget.offsetLeft * 1.5)) / (e.currentTarget.offsetWidth ) * 100));
+        if (this.props.seekBarStyle === "long") {
+            newPercentage = Math.floor((((e.clientX - windowSize) - (e.currentTarget.offsetLeft * 1.5)) / (e.currentTarget.offsetWidth) * 100));
+        } else if (this.props.seekBarStyle === "medium") {
+            newPercentage = Math.floor((((e.clientX - windowSize) + (e.currentTarget.offsetLeft * 107)) / (e.currentTarget.offsetWidth) * 100));
         } else {
             newPercentage = Math.floor((((e.clientX - windowSize) - (e.currentTarget.offsetLeft * 1.25)) / (e.currentTarget.offsetWidth) * 100));
         }
+        debugger;
         this.props.seekPercentage(newPercentage);
     }
 
