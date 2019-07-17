8b37c0ec33e7e5855d13b16dcb5fc83ec6b382c1
orenshaf <orenshaf@gmail.com>
2019-07-17 14:14:23 -0400

    fix logged out navbar

--- frontend/components/navbar/nav_bar.jsx
+++ frontend/components/navbar/nav_bar.jsx
@@ -39,7 +39,7 @@ class NavBar extends React.Component {
     
     toggleNavBar(){
         const footer = this.props.currentTrack ? <TrackPlayerContainer /> : null;
-        const profilePicture = <img className="profile-picture-nav" src={this.props.currentUser.photoUrl} />
+        const profilePicture = this.props.currentUser ? <img className="profile-picture-nav" src={this.props.currentUser.photoUrl} /> : null;
         if (this.props.currentUser && this.props.location.pathname !== '/logout') {
             return (
                 <>
